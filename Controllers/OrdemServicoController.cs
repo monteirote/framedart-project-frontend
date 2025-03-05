7f4db00397691272e59651243f5e860798d3477b
Vinícius Monteiro <vinicius@iconax.com.br>
2025-03-04 23:24:58 -0300

    feat: starting new page to add orders

---
+++ Controllers/OrdemServicoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace framedart_project_frontend.Controllers {
+
+    [Route("OS")]
+    public class OrdemServicoController : Controller {
+
+        [Route("Adicionar")]
+        public IActionResult AdicionarOS () {
+            return View();
+        }
+
+    }
+}

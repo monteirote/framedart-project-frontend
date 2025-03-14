3ae4b30d786250ba40db8cde3b384112398b875d
Vinícius Monteiro <vinicius@iconax.com.br>
2025-03-14 15:24:42 -0300

    feat: finalizing the interactivity of the add order view

---
+++ wwwroot/js/AdicionarOS_JS.js
@@ -0,0 +1,85 @@
+﻿function IniciarSelectsModal () {
+
+    $('#cdFundo').select2({
+        dropdownParent: $('#modalQuadro'),
+        placeholder: defaultPlaceholder,
+        minimumInputLength: 3,
+        ajax: {
+            url: '/Order/SearchMaterial',
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return { text: params.term, type: 'Background' };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.data
+                };
+            },
+            cache: true,
+            async: true
+        }
+    });
+    $('#cdMoldura').select2({
+        dropdownParent: $('#modalQuadro'),
+        placeholder: defaultPlaceholder,
+        minimumInputLength: 3,
+        ajax: {
+            url: '/Order/SearchMaterial',
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return { text: params.term, type: 'Frame' };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.data
+                };
+            },
+            cache: true,
+            async: true
+        }
+    });
+    $('#cdPapel').select2({
+        dropdownParent: $('#modalQuadro'),
+        placeholder: defaultPlaceholder,
+        minimumInputLength: 3,
+        ajax: {
+            url: '/Order/SearchMaterial',
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return { text: params.term, type: 'Paper' };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.data
+                };
+            },
+            cache: true,
+            async: true
+        }
+    });
+    $('#cdVidro').select2({
+        dropdownParent: $('#modalQuadro'),
+        placeholder: defaultPlaceholder,
+        minimumInputLength: 3,
+        ajax: {
+            url: '/Order/SearchMaterial',
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return { text: params.term, type: 'Glass' };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.data
+                };
+            },
+            cache: true,
+            async: true
+        }
+    });
+
+}
+
